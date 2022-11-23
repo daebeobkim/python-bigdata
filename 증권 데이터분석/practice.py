a3dda76b0ae8773c73bc3883e0c29faea9c0f06c
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2022-11-23 16:19:05 +0900

    Update practice.py

--- 증권 데이터분석/practice.py
+++ 증권 데이터분석/practice.py
@@ -5,5 +5,5 @@ yf.pdr_override()
 sec = pdr.get_data_yahoo('005930.KS', start='2018-05-04')
 msft = pdr.get_data_yahoo('hsft',start='2018-05-04')
 
-print(sec.head())
+print(sec.index)
 #내일 추가 코드해석하기
