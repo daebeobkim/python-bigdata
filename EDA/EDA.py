4618c96b3f7429f98ebaa8118217b92d500f996e
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-11 08:55:33 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -6,10 +6,12 @@ import pandas as pd
 d1 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_1.txt")
 d2 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_2.txt")
 
-cdf = np.linspace(1/d1.size,1,d1.size)
+cdf = np.linspace(1/d1.size,1,d1.size) #cdf는 누적 분포 함수
 sd1 = np.sort(d1)
 sd2 = np.sort(d2)
 plt.plot(sd1,cdf,label="D1 CDF")
 plt.plot(sd2,cdf,label="D2 CDF")
+plt.hist(d1,histtype="step",density=True,alpha=0.3)
+plt.hist(d2,histtype="step",density=True,alpha=0.3)
 plt.legend()
 plt.show()
