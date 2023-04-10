4931fcaf31d1d150d545a9d36eb26e6dc580f762
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-10 16:20:03 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -5,8 +5,9 @@ import pandas as pd
 
 d1 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_1.txt")
 d2 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_2.txt")
-print(d1.shape,d2.shape)
-
-plt.hist(d1,label="D1")
-plt.hist(d2,label="D2")
+bins = np.linspace(min(d1.min(),d2.min()),max(d1.max(),d2.max()),50)#순서대로 구간시작점, 구간끝점, 구간 내 숫자 갯수
+counts1,_,_ = plt.hist(d1,bins=bins,label="D1", density = True,histtype = "step", lw = 3)# density = True 사용시 %로 출력, 
+plt.hist(d2, bins=bins, label="D2",density = True,histtype = "step",ls=":")
+plt.legend()
+plt.ylabel("Probability")
 plt.show()
