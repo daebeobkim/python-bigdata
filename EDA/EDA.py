aa50f687bf31272f5fc721a9565166ea9b6945f4
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-11 19:19:03 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,17 +1,13 @@
 import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
+import pickle
 import pandas as pd
+import matplotlib.pyplot as plt
+d1 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_1d.txt")
+d2 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_2d.txt")
+d3 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_curve.txt")
+print(d1.shape,d2.shape)
 
-d1 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_1.txt")
-d2 = np.loadtxt("C:/Users/rlaeo/Downloads/1D/example_2.txt")
-
-cdf = np.linspace(1/d1.size,1,d1.size) #cdf는 누적 분포 함수
-sd1 = np.sort(d1)
-sd2 = np.sort(d2)
-plt.plot(sd1,cdf,label="D1 CDF")
-plt.plot(sd2,cdf,label="D2 CDF")
-plt.hist(d1,histtype="step",density=True,alpha=0.3)
-plt.hist(d2,histtype="step",density=True,alpha=0.3)
-plt.legend()
+plt.scatter(d1,np.random.normal(7,0.2,size=d1.size), s=1, alpha=0.5)
+#  random.normal 함수를 이용해 7을 평균값으로 하고 0.2를 표준편차로 하는 정규분포를 따르는 난수를 생성
+plt.scatter(d2[:,0],d2[:,1])
 plt.show()
