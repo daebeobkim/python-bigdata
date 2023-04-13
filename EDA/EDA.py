5f61178d8a7920dd73a66f6bec678e3ce4fa44b0
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-13 18:25:41 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -8,10 +8,6 @@ d3 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_curve.txt")
 
 
 xs,ys = d3.T #T를 붙혀 치환할 수 있도록 만듦
-p = np.polyfit(xs,ys,deg=5)  #np.polyfit을 입력해 다항식을 얻음, 5차 다항식이 되도록 deg=5를 입력, 이걸로 x값,x제곱값,x세제곱 등을 구할 수 있음
+p = np.polyval(xs,ys,deg=5)  #np.polyfit을 입력해 다항식을 얻음, 5차 다항식이 되도록 deg=5를 입력, 이걸로 x값,x제곱값,x세제곱 등을 구할 수 있음
 #y값을 다시 얻고자 하거나 다항식을 표본으로 추출하고자 한다면 np.polyval을 사용
-ps = np.polyval(p,xs)#을 입력하고 p,xs를 전달
-plt.plot(xs,ys,".",label="Data")
-plt.plot(xs,ps,label="Bad poly fit")
-plt.legend()
-plt.show()
+print(p)
