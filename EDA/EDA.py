708ce3054fb99ae2f560b980807fc7c3e642e6ef
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-17 21:07:04 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -1,13 +1,7 @@
 import numpy as np
-import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
 
-import numpy as np
-
-X = np.linspace(0, 2*np.pi)
-Y = np.sin(X)
-
-fig, ax = plt.subplots()
-ax.plot(X, Y)
+plt.plot([1,2,3,4],[12,43,25,15])
+plt.savefig('test.png')
 plt.show()
