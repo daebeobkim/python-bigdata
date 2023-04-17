37c846706f49ca179521db27317b11e46cb7462d
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-04-17 20:51:12 +0900

    Update EDA.py

--- EDA/EDA.py
+++ EDA/EDA.py
@@ -2,16 +2,12 @@ import numpy as np
 import pickle
 import pandas as pd
 import matplotlib.pyplot as plt
-d1 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_1d.txt")
-d2 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_2d.txt")
-d3 = np.loadtxt("C:/Users/rlaeo/Downloads/Outliers (1)/outlier_curve.txt")
 
-fig = plt.figure()
-x = [0,1,2,3,4]
-y = [10,25,30,8,30]
-fig, plt.plot(x,y,label='Test')
-plt.xlabel('x axis')
-plt.ylabel('y axis')
-plt.title('Main Title')
-plt.legend()
+import numpy as np
+
+X = np.linspace(0, 2*np.pi)
+Y = np.sin(X)
+
+fig, ax = plt.subplots()
+ax.plot(X, Y)
 plt.show()
