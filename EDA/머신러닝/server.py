f87c0c74009ec9958b242a685b88e03dd5d101f1
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-05-26 18:36:50 +0900

    Update server.py

--- EDA/머신러닝/server.py
+++ EDA/머신러닝/server.py
@@ -1,49 +1,27 @@
-from flask import Flask, jsonify, request
-import mysql.connector
+from flask import Flask, render_template
+from flask_sqlalchemy import SQLAlchemy
+from helloflask import Members
 
 app = Flask(__name__)
 
-# MySQL 연결 설정
-db = mysql.connector.connect(
-    host='127.0.0.1',
-    user='root',
-    password='0000',
-    db='customer',
+# database 설정파일
+app.config["SQLALCHEMY_DATABASE_URI"] = "mysql+pymysql://root:1234@localhost:3306/testdb"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+db = SQLAlchemy(app)
 
-    charset='utf8'
-)
-
-
-
-
-# 검색 기능 API
-@app.route('/', methods=['POST'])
-def search():
-    # 안드로이드 스튜디오에서 전송된 검색어 받기
-    search_word = request.form['search_word']
-
-    # MySQL에서 검색어 검색
-    cursor = db.cursor()
-    query = """SELECT * 
-    FROM food_table
-    WHERE food_name LIKE %s
-    ORDER BY 
-        CASE 
-            WHEN food_name LIKE %s THEN 0 
-            WHEN food_name LIKE %s THEN 1 
-            ELSE 2 
-        END"""
-    cursor.execute(query, ('%' + search_word + '%', search_word + '%', '%' + search_word + '%'))
-    results = cursor.fetchall()
-    cursor.close()
-
-
-    # 검색 결과를 JSON 형태로 변환하여 안드로이드 스튜디오에 전송
-    return jsonify(results)
-
-
-if __name__ == '__main__':
-    app.run()
-
-
+@app.route('/home')
+def home():
+    return render_template('home.html')
+    
+@app.route("/one")
+def home():
+	member = Members.query.first()
+	return 'Hello {0}, {1}, {2}, {3}, {4}'\
+		.format(member.name, member.email, member.phone, member.start.isoformat(), member.end.isoformat())
+	#return render_template('home.html')
+    
+@app.route('/all')
+def select_all():
+    members = Members.query.all()
+    return render_template('db.html', members=members)
 
