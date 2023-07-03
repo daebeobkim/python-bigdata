675d890dde1c14111a1318ada127e41666b65620
daebeobkim <109015772+daebeobkim@users.noreply.github.com>
2023-07-03 21:07:51 +0900

    Update 0628.py

--- spark/0628.py
+++ spark/0628.py
@@ -2,7 +2,18 @@ from pyspark import SparkConf, SparkContext
 import collections
 
 conf = SparkConf().setMaster("local").setAppName("RatingsHistogram")
+"""
+SparkConf() : 클래스의 인스턴스를 생성
+setMaster("local") : 애플리케이션을 실행할 spark 클러스터의 url을 설정, spark클러스터를 사용하지 않고
+로컬 모드로 설정하겠다는 의미
+setAppName("RatingsHistogram") : 애플리케이션의 이름설정
+"""
 sc = SparkContext(conf = conf)
 
 lines = sc.textFile("file:///SparkCourse/ml-100k/u.data")
-lines
+ratings = lines.map(lambda x: x.split()[2])
+result = ratings.countByValue() #데이터분할
+
+sortedResults = collections.OrderedDict(sorted(result.items()))
+for key, value in sortedResults.items():
+    print("%s %i" % (key, value))
